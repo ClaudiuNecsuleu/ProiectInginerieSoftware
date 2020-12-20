f0dabc406d85b5497a87b5d8e716fe319ca1e015
Clau <Clau@Clau>
2020-12-20 14:56:34 +0200

    servlet profile

---
+++ ProiectInginerieSoftware/src/main/java/com/ulbs/proiectingineriesoftware/Servlet/Profile/ProfileServlet.java
@@ -0,0 +1,95 @@
+package com.ulbs.proiectingineriesoftware.Servlet.Profile;
+
+import com.ulbs.proiectingineriesoftware.Common.UserBean;
+import com.ulbs.proiectingineriesoftware.Models.User;
+import com.ulbs.proiectingineriesoftware.Services.CommentDaoLocal;
+import com.ulbs.proiectingineriesoftware.Services.UserDaoLocal;
+import java.io.IOException;
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet(name = "ProfileServlet", urlPatterns = {"/ProfileServlet"})
+public class ProfileServlet extends HttpServlet {
+
+    @EJB
+    UserDaoLocal userDaoLocal;
+
+    @EJB
+    CommentDaoLocal commentDaoLocal;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        if (request != null) {
+            String username = request.getParameter("username");
+
+            HttpSession session = request.getSession(true);
+
+            UserBean currentUser = ((UserBean) (session.getAttribute("currentSessionUser")));
+
+            User user = userDaoLocal.getUserProfile(currentUser.getUsername());
+
+            request.setAttribute("user", user);
+
+            request.setAttribute("getCommentsByUsername", commentDaoLocal.getAllCommentsByUser(currentUser.getUsername()));
+        }
+
+        request.getRequestDispatcher("profile.jsp").forward(request, response);
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
